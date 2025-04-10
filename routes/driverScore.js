06d20886c2b29bd309e2ee8662cb924d08ac275c
James A <G00379271@atu.ie>
2025-04-10 15:44:15 +0100

    clean up

--- routes/driverScore.js
+++ routes/driverScore.js
@@ -4,8 +4,9 @@ const router = express.Router();
 router.post('/processFile', (req, res) => {
     const {processContent} = req.body
     if (!processContent) {
-        return res.json({success: false, content: processContent})
+        return res.json({success: false})
     }
+
     return res.json({success: true})
 });
 
