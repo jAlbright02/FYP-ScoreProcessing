d1def90d9dc7b1964f770205ec2b6a72282b2f8a
James A <G00379271@atu.ie>
2025-04-10 15:36:44 +0100

    code is connected up locally

--- routes/driverScore.js
+++ routes/driverScore.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.post('/processFile', (req, res) => {
+    const {processContent} = req.body
+    if (!processContent) {
+        return res.json({success: false, content: processContent})
+    }
+    return res.json({success: true})
+});
 
 module.exports = router;
